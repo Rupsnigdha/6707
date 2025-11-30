9e2cd5fa2ceda041e694e12f4cc97d926b0193c7
Rupsnigdha <rupsnigdhakashyap@gmail.com>
2025-11-30 16:53:50 +0530

    feat: add streamer images

--- components/pages/Streamers.tsx
+++ components/pages/Streamers.tsx
@@ -2,10 +2,14 @@
 
 import * as Section from "@/components/shared/Section";
 import { cn } from "@/lib/utils/cn";
+import DhimanImg from "@/assets/dhiman.jpg";
+import PingalImg from "@/assets/pingal.jpg";
 import { useState, type ComponentProps, type MouseEvent } from "react";
+import Image, { StaticImageData } from "next/image";
 
 interface Streamer {
-  image: string;
+  name: string;
+  image: StaticImageData;
   socials: {
     youtube: string;
     instagram: string;
@@ -14,21 +18,16 @@ interface Streamer {
 
 const streamers: Streamer[] = [
   {
-    image: "abc",
+    name: "Dhiman",
+    image: DhimanImg,
     socials: {
       youtube: "ac",
       instagram: "bc",
     },
   },
   {
-    image: "abc",
-    socials: {
-      youtube: "ac",
-      instagram: "bc",
-    },
-  },
-  {
-    image: "abc",
+    name: "Bondhu",
+    image: PingalImg,
     socials: {
       youtube: "ac",
       instagram: "bc",
@@ -44,8 +43,6 @@ interface StreamerCardProps extends ComponentProps<"div"> {
 }
 
 const StreamerCard = (props: StreamerCardProps) => {
-  const colStart = `col-start-${4 * props.position + 1}`;
-
   const [hovered, setHovered] = useState(false);
 
   const handleMouseEnter = () => {
@@ -59,16 +56,38 @@ const StreamerCard = (props: StreamerCardProps) => {
     <div
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className={cn("col-span-3 relative", colStart)}
+      className="relative"
     >
+      <a
+        className={cn(
+          "block absolute font-mansalva transition-all duration-300 text-2xl bg-white text-black px-10 py-4 rounded-2xl top-1/2 left-1/2 -translate-1/2 hover:bg-accent-red hover:text-white  rotate-12",
+          hovered && "translate-0 -left-30 top-4",
+        )}
+        href={props.streamer.socials.youtube}
+      >
+        youtube
+      </a>
+      <a
+        className={cn(
+          "absolute font-mansalva transition-all duration-300 text-2xl bg-white text-black px-10 py-4 rounded-2xl top-1/2 left-1/2 -translate-1/2 hover:bg-accent-red hover:text-white -rotate-12",
+          hovered && "translate-0 -left-30 top-24",
+        )}
+        href={props.streamer.socials.youtube}
+      >
+        instagram
+      </a>
       <div
         className={cn(
-          "aspect-video border-white border-4 -rotate-3 rounded-4xl transition-all duration-100 bg-blue-200",
+          "aspect-video border-white border-4 -rotate-3 rounded-4xl transition-all duration-100 overflow-hidden",
+
           hovered && "-rotate-6",
         )}
-      ></div>
-      <div className="absolute font-mansalva text-2xl bg-white text-black px-10 py-4 rounded-2xl top-4 left-0 -translate-x-[80%] hover:bg-red-300">
-        visit youtube
+      >
+        <Image
+          src={props.streamer.image}
+          className="h-full w-full object-cover"
+          alt={props.streamer.name}
+        />
       </div>
     </div>
   );
@@ -83,7 +102,7 @@ export const Streamers = () => {
           We live rent-free on your second monitor. Roster so diverse, we bet
           you’re watching one of us at any given time.
         </p>
-        <div className="grid grid-cols-11 gap-5 pt-24">
+        <div className="grid grid-cols-3 gap-36 pt-24">
           {streamers.map((streamer, i) => (
             <StreamerCard
               key={i}
